c550acdd8b38ec713bcd2d31ea5c3611302c86f2
yoni <yoni.melman@gmail.com>
2017-01-13 12:05:29 +0200

    basic version of aliasing

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
 var aliases = {
-"dana": "https://www.google.co.il/search?q=yoni+melman&gws_rd=cr,ssl&ei=hLJ2WOO_GpvhwAL0m7aoBw#q=dana+stolowicz",
-"dana2": "https://www.google.co.il/search?q=yoni+melman&gws_rd=cr,ssl&ei=hLJ2WOO_GpvhwAL0m7aoBw"
+    "google": "https://www.google.co.il/",
+    "ynet": "https://www.ynet.co.il/",
+    "a-ynet": "http://www.haaretz.co.il"
 };
 
 chrome.omnibox.onInputChanged.addListener(
@@ -32,19 +33,46 @@ chrome.omnibox.onInputStarted.addListener(function() {
 
 function updateDefaultSuggestion(text, suggest) {
     if (text != ''){
-        var suggestions = []
+        chrome.omnibox.setDefaultSuggestion(
+            {description: '<match>Alias</match>: Enter an alias'}
+        );
+        var match = '';
+        var url = '';
+        var suggestions = [];
         for (var key in aliases){
-            console.log("key: " + key);
-            if (key.startsWith(text)){
-                suggestions.push({content: key, description: aliases[key]});
-                console.log("in update suggestion");
+            url = "<url>" + varifyUrl(aliases[key]) + "</url>";
+            console.log("key: " + key + " text: " + text);
+            if (key == text){
+                console.log("key matched text");
+                chrome.omnibox.setDefaultSuggestion(
+                    {description: url}
+                );
+                return;
+            };
+            if (key.includes(text)){
+                match = key.replace(text, "<match>"+text+"</match>");
+
+                // var description = "<match>" + text "</match">
+                suggestions.push({
+                    content: key,
+                    description: match + ": " + url
+                });
               };
             };
+        console.log("presenting " + suggestions.length + " suggestions");
         suggest(suggestions);
         };
     };
 
-
+function varifyUrl(url){
+    var fixed_url = url
+    fixed_url = fixed_url.replace('"', "&quot;")
+    fixed_url = fixed_url.replace("'", "&apos;")
+    fixed_url = fixed_url.replace('<', "&lt;")
+    fixed_url = fixed_url.replace('>', "&gt;")
+    fixed_url = fixed_url.replace('&', "&amp;")
+    return fixed_url
+}
 
 
 // chrome.omnibox.onInputCancelled.addListener(function() {
